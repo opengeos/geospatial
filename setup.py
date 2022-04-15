a67ef11ce029c9a68da3cbd922c9d50f1a2f91b0
Qiusheng Wu <giswqs@gmail.com>
2022-04-15 00:30:31 -0400

    Bump version: 0.5.4 → 0.5.5

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/geospatial',
-    version='0.5.4',
+    version='0.5.5',
     zip_safe=False,
 )
