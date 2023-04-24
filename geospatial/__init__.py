4e16c4b0b1d11e7ed84c254c88bab2b8a9b3583b
Qiusheng Wu <giswqs@gmail.com>
2023-04-24 00:32:54 -0400

    Bump version: 0.7.1 → 0.8.0

--- geospatial/__init__.py
+++ geospatial/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = 'giswqs@gmail.com'
-__version__ = '0.7.1'
+__version__ = '0.8.0'
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/opengeos/geospatial',
-    version='0.7.1',
+    version='0.8.0',
     zip_safe=False,
 )
